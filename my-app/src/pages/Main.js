d84e4cc55dc1488e8e4a1e77285c9f038cf5b5b8
rinaZC <rinachen207@gmail.com>
2022-03-14 00:37:20 -0400

    Main page frontend

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -1,9 +1,60 @@
-import * as React from "react";
+import * as React from 'react';
+import Post from '../components/Post';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Refresh from '@mui/icons-material/Refresh';
+import AddBox from '@mui/icons-material/AddBox';
+
+function TopBar() {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="sticky">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              App Name
+            </Typography>
+            
+            <IconButton
+                size="large"
+                color="inherit"
+              >
+                <Refresh />
+                <Typography variant="button">Refresh</Typography>
+            </IconButton>
+            <IconButton
+                size="large"
+                color="inherit"
+              >
+                <AddBox/>
+                <Typography variant="button">Make A Post</Typography>
+            </IconButton>
+            <IconButton
+                size="large"
+                color="inherit"
+              >
+                <AccountCircle />
+                <Typography variant="button">Profile</Typography>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
+
+let posts = [{}, {}, {}, {}, {}]
+
 
 export default function Main() {
     return (
         <div>
-            <h1>Main Page</h1>
+            <TopBar/>
+            {posts.map((item, index) => {
+                return <Post/>
+            })}
         </div>
     )
 }
