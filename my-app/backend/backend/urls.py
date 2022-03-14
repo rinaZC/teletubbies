05d076b5beb507845901e26a6a4ad91337151896
Perper <perper@umich.edu>
2022-03-13 23:21:55 -0400

    sign_up post_method&backend route set up

--- my-app/backend/backend/urls.py
+++ my-app/backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import account
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', account.index),
 ]
